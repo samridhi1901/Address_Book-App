53a6971f9dec4c67fa420ee9039852890766d279
samridhi singh <gurjarsam6@gmail.com>
2025-03-07 14:15:32 +0530

    UC5

--- src/main/java/com/example/AddressBook/model/AddressBookModel.java
+++ src/main/java/com/example/AddressBook/model/AddressBookModel.java
@@ -26,6 +26,12 @@ public class AddressBookModel {
     public String getEmail() {
         return email;
     }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public void setName(String name) {
         this.name = name;
     }
