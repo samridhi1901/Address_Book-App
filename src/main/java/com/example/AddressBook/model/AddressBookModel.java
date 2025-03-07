98b510f4ef5fb180355cb77433a67d1ad2ad826f
samridhi singh <gurjarsam6@gmail.com>
2025-03-07 14:01:11 +0530

    UC3

---
+++ src/main/java/com/example/AddressBook/model/AddressBookModel.java
@@ -0,0 +1,38 @@
+package com.example.AddressBook.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "addressbook")
+public class AddressBookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phone;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
