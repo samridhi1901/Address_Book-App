8fa741a3de7e2b6be65a0c7d6edf6cf4ed788106
samridhi singh <gurjarsam6@gmail.com>
2025-03-08 15:58:50 +0530

    UC9

--- src/main/java/com/example/AddressBook/dto/AddressBookDTO.java
+++ src/main/java/com/example/AddressBook/dto/AddressBookDTO.java
@@ -1,21 +1,20 @@
 package com.example.AddressBook.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Data
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class AddressBookDTO {
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z ]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
     private String name;
+
     private String phone;
     private String email;
-
-    public AddressBookDTO(String name, String phone, String email) {
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-    }
-
 
     public String getName() {
         return name;
